5f241be9a7c64701d0bbbb4a5b4817f0e64fe9f8
Spencer Ogden <spencer@spencerogden.com>
2018-08-28 21:06:57 -0400

    Switch to unittest for FT

--- functional_tests.py
+++ functional_tests.py
@@ -1,13 +1,22 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
 
-# Edith has heard about a cool new online to-do app. 
-# Sho goes to check out its homepage
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
+	def setUp(self):
+		self.browser = webdriver.Firefox()
 
-#She notices the page title and header mention to-do lists
-assert 'To-Do' in browser.title
+	def tearDown(self):
+		self.browser.quit()
+		
+	def test_can_start_a_list_and_retrieve_it_later(self):
+		# Edith has heard about a cool new online to-do app. 
+		# She goes to check out its homepage
+		self.browser.get('http://localhost:8000')
+
+		#She notices the page title and header mention to-do lists
+		self.assertIn('To-Do', self.browser.title)
+		self.fail('Finish the test!')
 
 # She is invited to enter a to-do item straight away
 
@@ -29,7 +38,7 @@ assert 'To-Do' in browser.title
 
 # She goes back to sleep
 
-browser.quit()
-
+if __name__ == '__main__':
+	unittest.main(warnings='ignore')
 
 
