3ccd66b08af8c23c1e46e820be263496edb35610
Spencer Ogden <spencer@spencerogden.com>
2018-09-08 22:31:28 -0400

    Added list model and foreign key relationship to Item

--- lists/views.py
+++ lists/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import redirect,render
 from django.http import HttpResponse
 
-from lists.models import Item
+from lists.models import Item,List
 
 
 # Create your views here.
@@ -9,8 +9,10 @@ def home_page(request):
 	return render(request,'home.html')
 	
 def new_list(request):
+	list_ = List.objects.create()
 	Item.objects.create(
-			text=request.POST.get('item_text',''))
+			text=request.POST.get('item_text',''),
+			list=list_)
 	return redirect('/lists/the-only-list-in-the-world/')
 	
 def view_list(request):
