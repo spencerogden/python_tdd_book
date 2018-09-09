584dfb2c2ec22043f6411463e8b8bd5e9c1c8c7a
Spencer Ogden <spencer@spencerogden.com>
2018-09-09 11:58:28 -0400

    new URL and view for adding to existing lists. FT Pass

--- lists/views.py
+++ lists/views.py
@@ -13,9 +13,13 @@ def new_list(request):
 	Item.objects.create(
 			text=request.POST.get('item_text',''),
 			list=list_)
-	return redirect('/lists/the-only-list-in-the-world/')
+	return redirect(f'/lists/{list_.id}/')
 	
-def view_list(request):
-	items = Item.objects.all()
-	return render(request,'list.html', {'items':items})
+def view_list(request,list_id):
+	list_ = List.objects.get(id=list_id)
+	return render(request,'list.html', {'list':list_})
 	
+def add_item(request,list_id):
+	list_ = List.objects.get(id=list_id)
+	Item.objects.create(text=request.POST['item_text'], list=list_)
+	return redirect(f'/lists/{list_.id}/')
