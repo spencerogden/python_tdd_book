e6d9e5d3c7f03b1538f6b395c0d88d4c39c01e3a
Spencer Ogden <spencer@spencerogden.com>
2018-09-08 22:04:21 -0400

    New URL, view and template for displayng lists

--- lists/tests.py
+++ lists/tests.py
@@ -22,20 +22,11 @@ class HomePageTest(TestCase):
         response = self.client.post('/',data={'item_text':"A new list item"})
         
         self.assertEqual(response.status_code, 302)
-        self.assertEqual(response['location'],'/')
+        self.assertEqual(response['location'],'/lists/the-only-list-in-the-world/')
     
     def test_only_saves_items_when_neccessary(self):
         self.client.get('/')
         self.assertEqual(Item.objects.count(), 0)
-        
-    def test_displays_all_list_items(self):
-    	Item.objects.create(text='itemey 1')
-    	Item.objects.create(text='itemey 2')
-    	
-    	response = self.client.get('/')
-    	
-    	self.assertIn('itemey 1', response.content.decode())
-    	self.assertIn('itemey 2', response.content.decode())
         
 class ItemModelTest(TestCase):
     def test_saving_and_retreiving_items(self):
@@ -56,3 +47,18 @@ class ItemModelTest(TestCase):
             'The first (ever) list item')
         self.assertEqual(second_saved_item.text,
             'Item the second')
+
+class ListViewTest(TestCase):
+    def test_displays_all_items(self):
+        Item.objects.create(text='itemey 1')
+        Item.objects.create(text='itemey 2')
+        
+        response = self.client.get('/lists/the-only-list-in-the-world/')
+        
+        self.assertContains(response,'itemey 1')
+        self.assertContains(response,'itemey 2')
+        
+    def test_uses_list_template(self):
+    	response = self.client.get('/lists/the-only-list-in-the-world/')
+    	self.assertTemplateUsed(response,'list.html')
+        
