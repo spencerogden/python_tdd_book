fdd7b122f2fb5ad03d547d69aece27e23502604d
Spencer Ogden <spencer@spencerogden.com>
2018-09-08 22:14:58 -0400

    New URL, view and template for displayng lists

--- lists/views.py
+++ lists/views.py
@@ -13,6 +13,11 @@ def home_page(request):
 
 	return render(request,'home.html')
 	
+def new_list(request):
+	Item.objects.create(
+			text=request.POST.get('item_text',''))
+	return redirect('/lists/the-only-list-in-the-world/')
+	
 def view_list(request):
 	items = Item.objects.all()
 	return render(request,'list.html', {'items':items})
