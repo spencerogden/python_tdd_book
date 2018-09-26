ac827f0622f4aa0abbb61f1425f8bd774804c0d1
Spencer Ogden <spencer@spencerogden.com>
2018-09-25 22:06:09 -0400

    Refactor hardcoded URLs out of templates

--- lists/views.py
+++ lists/views.py
@@ -8,7 +8,7 @@ from lists.models import Item,List
 # Create your views here.
 def home_page(request):
 	return render(request,'home.html')
-	
+
 def new_list(request):
 	list_ = List.objects.create()
 	item = Item.objects.create(
