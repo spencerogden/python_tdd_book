0dfbdb160862bdad7e54c17757c12d3bdc1e3c92
Spencer Ogden <spencer@spencerogden.com>
2018-09-26 22:03:54 -0400

    use form in all views, back to working state

--- lists/forms.py
+++ lists/forms.py
@@ -2,6 +2,8 @@ from django import forms
 
 from lists.models import Item
 
+EMPTY_ITEM_ERROR = "You can't have an empty list item"
+
 class ItemForm(forms.ModelForm):
     class Meta:
         model = Item
--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -4,7 +4,7 @@ from django.http import HttpRequest
 
 from lists.views import home_page  
 from lists.models import Item, List
-from lists.forms import ItemForm
+from lists.forms import ItemForm, EMPTY_ITEM_ERROR
 
 from django.utils.html import escape
 
@@ -15,10 +15,17 @@ class HomePageTest(TestCase):
         self.assertTemplateUsed(response, 'home.html')
         
     def test_home_page_uses_item_form(self):
-    	response = self.client.get('/')
-    	self.assertIsInstance(response.context['form'], ItemForm)
+        response = self.client.get('/')
+        self.assertIsInstance(response.context['form'], ItemForm)
 
 class ListViewTest(TestCase):
+    def post_invalid_input(self):
+        list_ = List.objects.create()
+        return self.client.post(
+            f'/lists/{list_.id}/',
+            data={'text': ''}
+        )
+        
     def test_displays_only_items_for_that_list(self):
         correct_list = List.objects.create()
         Item.objects.create(text='itemey 1',list=correct_list)
@@ -100,7 +107,29 @@ class ListViewTest(TestCase):
         expected_error = escape("You can't have an empty list item")
         self.assertContains(response, expected_error)
     
-        
+    def test_displays_item_form(self):
+        list_ = List.objects.create()
+        response = self.client.get(f'/lists/{list_.id}/')
+        self.assertIsInstance(response.context['form'],ItemForm)
+        self.assertContains(response, 'name="text"')
+            
+    def test_for_invalid_input_nothing_saved_to_db(self):
+        self.post_invalid_input()
+        self.assertEqual(Item.objects.count(), 0)
+
+    def test_for_invalid_input_renders_list_template(self):
+        response = self.post_invalid_input()
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'list.html')
+
+    def test_for_invalid_input_passes_form_to_template(self):
+        response = self.post_invalid_input()
+        self.assertIsInstance(response.context['form'], ItemForm)
+
+    def test_for_invalid_input_shows_error_on_page(self):
+        response = self.post_invalid_input()
+        self.assertContains(response, escape(EMPTY_ITEM_ERROR))
+            
 class NewListTest(TestCase):
     def test_can_save_a_POST_request(self):
         self.client.post('/lists/new',data={'text':"A new list item"})
@@ -114,13 +143,19 @@ class NewListTest(TestCase):
         response = self.client.post('/lists/new',data={'text':"A new list item"})
         new_list = List.objects.first()
         self.assertRedirects(response,f'/lists/{new_list.id}/')
-        
-    def test_validation_errors_are_sent_back_to_home_page_template(self):
-        response = self.client.post('/lists/new', data={'text':''})
+
+    def test_for_invalid_input_renders_home_template(self):
+        response = self.client.post('/lists/new', data={'text': ''})
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'home.html')
-        expected_error = escape("You can't have an empty list item")
-        self.assertContains(response, expected_error)
+
+    def test_validation_errors_are_shown_on_home_page(self):
+        response = self.client.post('/lists/new', data={'text': ''})
+        self.assertContains(response, escape(EMPTY_ITEM_ERROR))
+
+    def test_for_invalid_input_passes_form_to_template(self):
+        response = self.client.post('/lists/new', data={'text': ''})
+        self.assertIsInstance(response.context['form'], ItemForm)
         
     def test_invalid_list_items_arent_saved(self):
         self.client.post('/lists/new', data={'text': ''})
