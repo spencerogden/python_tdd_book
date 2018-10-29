2ce6ec3bc961a672725d45180190df6eaf3ffd6c
Spencer Ogden <spencer@spencerogden.com>
2018-10-28 21:24:12 -0400

    Complete form based validation

--- lists/forms.py
+++ lists/forms.py
@@ -4,7 +4,7 @@ from django.core.exceptions import ValidationError
 from lists.models import Item
 
 EMPTY_ITEM_ERROR = "You can't have an empty list item"
-DUPLICATE_ITEM_ERROR = "You've already got this in you list"
+DUPLICATE_ITEM_ERROR = "You've already got this in your list"
 
 class ItemForm(forms.ModelForm):
     class Meta:
@@ -35,4 +35,7 @@ class ExistingListItemForm(ItemForm):
         except ValidationError as e:
             e.error_dict = {'text':[DUPLICATE_ITEM_ERROR]}
             self._update_errors(e)
+    
+    def save(self):
+        return forms.models.ModelForm.save(self)
             
--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -4,7 +4,10 @@ from django.http import HttpRequest
 
 from lists.views import home_page  
 from lists.models import Item, List
-from lists.forms import ItemForm, EMPTY_ITEM_ERROR
+from lists.forms import (
+	DUPLICATE_ITEM_ERROR, EMPTY_ITEM_ERROR,
+    ExistingListItemForm, ItemForm,
+    )
 
 from django.utils.html import escape
 
@@ -112,7 +115,7 @@ class ListViewTest(TestCase):
     def test_displays_item_form(self):
         list_ = List.objects.create()
         response = self.client.get(f'/lists/{list_.id}/')
-        self.assertIsInstance(response.context['form'],ItemForm)
+        self.assertIsInstance(response.context['form'],ExistingListItemForm)
         self.assertContains(response, 'name="text"')
             
     def test_for_invalid_input_nothing_saved_to_db(self):
@@ -132,7 +135,6 @@ class ListViewTest(TestCase):
         response = self.post_invalid_input()
         self.assertContains(response, escape(EMPTY_ITEM_ERROR))
     
-    @skip
     def test_duplicate_item_validation_errors_end_up_on_lists_page(self):
         list1 = List.objects.create()
         item1 = Item.objects.create(list=list1, text='textey')
@@ -141,7 +143,7 @@ class ListViewTest(TestCase):
             data={'text': 'textey'}
         )
 
-        expected_error = escape("You've already got this in your list")
+        expected_error = escape(DUPLICATE_ITEM_ERROR)
         self.assertContains(response, expected_error)
         self.assertTemplateUsed(response, 'list.html')
         self.assertEqual(Item.objects.all().count(), 1)
