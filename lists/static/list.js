658415ff75097370fe4aaedc02223ed45032eebe
Spencer Ogden <spencer@spencerogden.com>
2018-10-28 22:23:37 -0400

    wrap js code in a namespace

--- lists/static/list.js
+++ lists/static/list.js
@@ -1,4 +1,7 @@
-var initialize = function() {
+window.Superlists = {};
+
+
+window.Superlists.initialize = function() {
   console.log('initialization called')
   $('input[name="text"]').on('keypress',function(){
     $('.has-error').hide();
