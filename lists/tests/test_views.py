68ccf1763307a03adfe62e269ab7cde46afcd291
Spencer Ogden <spencer@spencerogden.com>
2018-10-28 20:59:34 -0400

    Do form validation for unique items

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -8,6 +8,8 @@ from lists.forms import ItemForm, EMPTY_ITEM_ERROR
 
 from django.utils.html import escape
 
+from unittest import skip
+
 class HomePageTest(TestCase):
     
     def test_home_page_returns_correct_html(self):
@@ -129,6 +131,20 @@ class ListViewTest(TestCase):
     def test_for_invalid_input_shows_error_on_page(self):
         response = self.post_invalid_input()
         self.assertContains(response, escape(EMPTY_ITEM_ERROR))
+    
+    @skip
+    def test_duplicate_item_validation_errors_end_up_on_lists_page(self):
+        list1 = List.objects.create()
+        item1 = Item.objects.create(list=list1, text='textey')
+        response = self.client.post(
+            f'/lists/{list1.id}/',
+            data={'text': 'textey'}
+        )
+
+        expected_error = escape("You've already got this in your list")
+        self.assertContains(response, expected_error)
+        self.assertTemplateUsed(response, 'list.html')
+        self.assertEqual(Item.objects.all().count(), 1)
             
 class NewListTest(TestCase):
     def test_can_save_a_POST_request(self):
