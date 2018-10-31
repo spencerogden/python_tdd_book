6cdee8973755a5fe6ebb149f1252fe417845fc5c
Spencer Ogden <spencer@spencerogden.com>
2018-10-30 23:11:00 -0400

    Create and return tokens on login attempts

--- accounts/views.py
+++ accounts/views.py
@@ -1,12 +1,20 @@
 from django.shortcuts import redirect
 from django.core.mail import send_mail
 from django.contrib import messages
+from django.core.urlresolvers import reverse
+
+from accounts.models import Token
 
 def send_login_email(request):
     email = request.POST['email']
+    token = Token.objects.create(email=email)
+    uri = request.build_absolute_uri(
+        reverse('login') + '?token=' + str(token.uid)
+    )
+    
     send_mail(
         'Your login link for Superlists',
-        'body text tbc',
+        f'Use this link to log in:\n\n{uri}',
         'noreply@superlists',
         [email],
     )
@@ -14,4 +22,7 @@ def send_login_email(request):
         request,
         "Check your email, we've sent you a link you can use to log in."
     )
+    return redirect('/')
+    
+def login(request):
     return redirect('/')
